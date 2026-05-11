14af2bbb9e7e531b7721e6bbb9e6214eeeefffe7
CokeAnd1ce <22373442@buaa.edu.cn>
2026-05-11 14:30:46 +0800

    h100 path

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,12 @@
 torchrun --standalone --nnodes 1 --nproc-per-node 2 scripts/pretrain.py \
     --model.type prism-jepasiglip+0_5b \
     --dataset.type llava-v15 \
-    --dataset.dataset_root_dir /home/jwhe/linyihan/datasets \
-    --model.vision_checkpoint_path /home/jwhe/linyihan/CKPT/vjepa2_1_vitl_384.pt \
-    --model.llm_local_path /home/jwhe/linyihan/CKPT/Qwen2.5-0.5B \
+    --dataset.dataset_root_dir /ssd/linyihan/datasets \
+    --model.vision_checkpoint_path /ssd/linyihan/ckpt/vjepa2_1_vitl_dist_vitG_384.pt \
+    --model.llm_local_path /ssd/linyihan/ckpt/Qwen2.5-0.5B \
     --stage finetune \
     --model.finetune_per_device_batch_size 4 \
     --run_root_dir ./runs \
-    --trackers '["jsonl","wandb"]' \
-    --wandb_project JEPA_SigLIP_pretrain \
-    --wandb_entity 22373442
+    --trackers '["jsonl"]' \
+    # --wandb_project JEPA_SigLIP_pretrain \
+    # --wandb_entity 22373442
