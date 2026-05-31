f070375bdd2e3e92d1c1ab2e13e04f063ca456ec
Coke_And_1ce <22373442@buaa.edu.cn>
2026-05-31 16:10:30 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 # export CUDA_VISIBLE_DEVICES=0,1,2,3
 torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
-    --model.type prism-qwen3-vjepa21-vitg-384px+1_7b \
-    --dataset.type llava-lvis4v-lrv \
+    --model.type prism-qwen3-vjepa21-vitl-384px+1_7b \
+    --dataset.type llava-v15 \
     --dataset.dataset_root_dir /ssd/linyihan/datasets \
     --model.vision_checkpoint_path /ssd/linyihan/ckpt/vjepa2_1_vitl_dist_vitG_384.pt \
     --model.llm_local_path /ssd/linyihan/ckpt/Qwen3-1.7B-Base \
@@ -10,5 +10,5 @@ torchrun --standalone --nnodes 1 --nproc-per-node 8 scripts/pretrain.py \
     --model.finetune_global_batch_size 32 \
     --run_root_dir ./runs \
     --trackers '["jsonl", "wandb"]' \
-    --wandb_project VJEPA_Qwen3_full_pretrain \
+    --wandb_project VJEPA_Qwen3_LLaVA_pretrain \
     --wandb_entity 22373442
